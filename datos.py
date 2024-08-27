0646203e61e7ef4ddef579fd717dc1f8a0d92ac2
SANTTYAGO <Santyagoelpro@gmail.com>
2024-08-27 02:44:21 -0500

    First commit

---
+++ datos.py
@@ -0,0 +1,9 @@
+productos = [
+    {'nombre': 'Celular', 'precio': 1250},
+    {'nombre': 'Computador', 'precio': 2150},
+    {'nombre': 'Tablet', 'precio': 875},
+    {'nombre': 'Consola', 'precio': 1650},
+    {'nombre': 'Lapiz', 'precio': 1250},
+    {'nombre': 'Borrador', 'precio': 1250},
+    {'nombre': 'Sacapuntas', 'precio': 1250},
+]
---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+from datos import productos
+
+app = Flask (__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html', productos=productos)
+    #return "<H1>Hola Mundito!!!</H1>"
+
+@app.route("/producto/<string:nombre>")
+def producto(nombre):
+    prod=[p for p in productos if p['nombre']==nombre]
+    return render_template('producto.html', producto=prod[0])
+
+app.run(host='0.0.0.0', port=8000, debug=True)
+
